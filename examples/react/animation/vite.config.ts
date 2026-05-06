07943badd8c45762b10c71ae226fab1300029c1a
Justin Walsh <contact.me@thejustinwalsh.com>
2026-05-06 01:06:41 -0400

    feat: enable Flatland devtools in Vite config for all examples

--- examples/react/animation/vite.config.ts
+++ examples/react/animation/vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => ({
   resolve: { conditions: ['source'] },
+  define: {
+    'import.meta.env.VITE_FLATLAND_DEVTOOLS': JSON.stringify('true'),
+  },
   plugins: [react()],
   base: command === 'serve' ? '/react/animation/' : './',
   server: {
